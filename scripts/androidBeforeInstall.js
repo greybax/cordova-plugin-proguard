85cc3e98e403a9195f47c20937f027e2aae6921a
Eric <info@ericparlier.com>
2025-04-01 09:59:14 +0200

    Merge proguard-custom.txt at after prepare, don't overwrite plugin file

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -3,20 +3,19 @@ let fs = require('fs');
 let path = require('path');
 
 module.exports = function (ctx) {
-  const projectRoot = ctx.opts.projectRoot;
-  const pluginDir = ctx.opts.plugin.dir;
-  const targetProguardFile = path.join(pluginDir, 'proguard-custom.txt');
-  const projectProguardFile = path.join(projectRoot, 'proguard-custom.txt');
-
-  try {
-    if (fs.existsSync(projectProguardFile)) {
-      const data = fs.readFileSync(projectProguardFile, 'utf8');
-      fs.appendFileSync(targetProguardFile, data);
-      console.log('Added optional proguard-rules to proguardFile.');
-    } else {
-      console.log('No optional proguard-custom.txt found in projectRoot: ' + projectRoot);
-    }
-  } catch(err) {
-    console.error(err);
-  }
+    const projectRoot = ctx.opts.projectRoot;
+    const pluginDir = ctx.opts.plugin.dir;
+    const sourceProguardFile = path.join(pluginDir, 'proguard-custom.txt');
+    const projectProguardFile = path.join(projectRoot, 'proguard-custom.txt');
+    const destProguardFile = path.join(projectRoot, '/platforms/android/app/src/main/assets/www/proguard-custom.txt');
+    try {
+        let baseFile = fs.readFileSync(sourceProguardFile, 'utf8');
+        if (fs.existsSync(projectProguardFile)) {
+            const customFile = fs.readFileSync(projectProguardFile, 'utf8');
+            baseFile += '\r\n'+customFile;
+        }
+        fs.writeFileSync(destProguardFile, baseFile);
+    }catch(err) {
+        console.error(err);
+    }  
 };
